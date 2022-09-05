4f0f77d1796d0c07de49da6e942dcfebf8357b44
IM <iva104@gmail.com>
2022-09-05 21:39:34 +0300

    Added settings file.

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Class to store settings for CatLive game"""
+    
+    def __init__(self):
+        self.bg_color = (230, 230, 230)
+        self.screen_width = 700
+        self.screen_height = 500
+        
+        self.cat_speed = 0.5
