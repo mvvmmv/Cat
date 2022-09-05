6bf213b5863e1fe054a901b87929c1f6e9908bf5
IM <iva104@gmail.com>
2022-09-05 21:40:04 +0300

    Cat now walks and change direction.

--- cat.py
+++ cat.py
@@ -1,4 +1,58 @@
+import pygame
+
 class Cat():
     """Create a cat"""
     def __init__(self, cl_game):
-        pass
+        self.screen = cl_game.screen
+        self.screen_rect = cl_game.screen.get_rect()
+        
+        self.settings = cl_game.settings
+        
+        # Uploads cat looking left image and gets rectangle.
+        self.image = pygame.image.load('images/cat.png')
+        self.rect = self.image.get_rect()
+        
+        # Uploads cat looking right image and gets rectangle.
+        self.image_right = pygame.image.load('images/cat_inverse.png')
+        # self.rect_right = self.image_right.get_rect()
+        
+        # The cat appears at the buttom of the screen.
+        self.rect.center = self.screen_rect.center
+        
+        # Saving coordinates of the cat's center.
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+        
+        # Relocation flag
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+        self.cat_looks_right = False
+    
+    def blitme(self):
+        """Draw the cat at the current position"""
+        if not self.cat_looks_right:
+            self.screen.blit(self.image, self.rect)
+        elif self.cat_looks_right:
+            self.screen.blit(self.image_right, self.rect)
+    
+    def update(self):
+        """Update location of the cat in regards with flag's value"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.cat_speed
+            self.cat_looks_right = True
+        
+        if self.moving_left and self.rect.left > 100:
+            self.x -= self.settings.cat_speed
+            self.cat_looks_right = False
+        
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.cat_speed
+            
+        if self.moving_down and self.rect.bottom < self.screen_rect.height - 100:
+            self.y += self.settings.cat_speed
+        
+        # Update attribute rect in regards with self.x, self.y.
+        self.rect.x = self.x
+        self.rect.y = self.y
--- cat_live.py
+++ cat_live.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
+
 from cat import Cat
+from settings import Settings
 
 class CatLive:
     """Class for managing cat's life"""
@@ -8,7 +10,8 @@ class CatLive:
     def __init__(self):
         pygame.init()
         
-        self.screen = pygame.display.set_mode((600,400))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         
         pygame.display.set_caption("Cat's life")
         
@@ -18,13 +21,50 @@ class CatLive:
         """Run main cicle of the game"""
         while True:
             self._check_events()
-    
+            self.cat.update()
+            self._update_screen()
+            
     def _check_events(self):
         """Handles keyboard and mouse events"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
+            elif event.type == pygame.KEYDOWN:
+                self._check_key_down_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_key_up_events(event)
+                
+    def _check_key_down_events(self, event):
+        """Reacts to keyboard button down events"""
+        if event.key == pygame.K_RIGHT:
+            self.cat.moving_right = True
+        elif event.key == pygame.K_ESCAPE:
+                sys.exit()
+        elif event.key == pygame.K_LEFT:
+            self.cat.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.cat.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.cat.moving_down = True
+    
+    def _check_key_up_events(self, event):
+        """Reacts to keyboard button up events"""
+        if event.key == pygame.K_RIGHT:
+            self.cat.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.cat.moving_left = False
+        if event.key == pygame.K_UP:
+            self.cat.moving_up = False
+        if event.key == pygame.K_DOWN:
+            self.cat.moving_down = False
+            
+    def _update_screen(self):
+        """Updates screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.cat.blitme()
+        
+        pygame.display.flip()
+        
 if __name__ == '__main__':
     # Create instance and run the game.
     cl = CatLive()
